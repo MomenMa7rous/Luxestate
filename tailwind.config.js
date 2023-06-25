d086ef54337361b881ed7f9e411a39276bd12292
momenma7rous <momenma7rous@gmail.com>
2023-06-25 22:37:04 +0300

    Adding Images & Doing The Initial Customizations.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 module.exports = {
   content: ['./public/*.html'],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {
+      colors: {
+        primary: '#1F373D',
+        secondary: '#FFCC01'
+      },
+      fontFamily: {
+        "main": ['Montserrat', 'sans-serif']
+      },
       container: {
-        center: true
+        center: true,
+        padding: '20px'
       }
     },
   },
