028cad927990b3651612cbed8e3cf1b42bf71976
momenma7rous <momenma7rous@gmail.com>
2023-06-26 18:36:31 +0300

    Scroll Progress Created & Some Enhancements.

--- public/js/main.js
+++ public/js/main.js
@@ -19,3 +19,22 @@ Array.from(mobileQuickActions.children).forEach(a => {
     mobileNav.classList.toggle("translate-x-0")
   })
 })
+
+// Header Border Fill On Scroll
+const headerBorder = document.getElementById('header-border')
+
+controlHeaderBorder()
+
+window.addEventListener("scroll", () => {
+  controlHeaderBorder()
+})
+
+function controlHeaderBorder() {
+  if (window.scrollY >= 20) {
+    headerBorder.style.opacity = '1'
+  } else {
+    headerBorder.style.opacity = '0'
+  }
+  let percentage = (((window.scrollY + window.innerHeight) / document.body.clientHeight) * 100)
+  headerBorder.style.width = `${percentage}%`
+}
